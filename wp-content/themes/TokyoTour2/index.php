5a4116a19d0808c002bdf52db3805a2c3298e80c
RuiOkazaki <okazaki200226@gmail.com>
2021-12-16 12:09:55 +0900

    update: 片側のコンテンツを取得

--- wp-content/themes/TokyoTour2/index.php
+++ wp-content/themes/TokyoTour2/index.php
@@ -1,9 +1,13 @@
 <?php get_header(); ?>
 <main id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
+<article <?php post_class(); ?>>
+  <h3 id="id-<?php the_ID(); ?>"><?php the_title(); ?></h3>
+  <date><?php echo get_the_date(); ?></date>
+  <p><?php the_content(); ?></p>
+  <?php the_post_thumbnail("thumbnail"); ?>
+</article>
 <?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+<?php wp_reset_query(); ?>
 </main>
 <?php get_footer(); ?>
